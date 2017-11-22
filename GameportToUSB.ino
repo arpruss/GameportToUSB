8297aeee95ddc216e748614d195bd035b924b42f
arpruss <arpruss@gmail.com>
2017-11-21 22:44:53 -0600

    foo

--- GameportToUSB.ino
+++ GameportToUSB.ino
@@ -9,7 +9,7 @@
 // Either gameport or Keyboard+Mouse+gameport mode. 
 //
 // Pinout for reading gameport analog values:
-// Gameport 1 (X1) A0 --10K-- ground (X)
+// Gameport 3 (X1) A0 --10K-- ground (X)
 // Gameport 6 (Y1) A1 --10K-- ground (Y)
 // Gameport 11 (X2) A2 --10K-- ground (slider 1)
 // Gameport 13 (Y2) A3 --10K-- ground (Z rotate)
@@ -20,8 +20,10 @@
 // Gameport 14 (B3) -- A6 (button 3)
 // Gameport 10 (B4) -- A7 (button 4)
 
+const uint32_t R2 = 10000;
+const uint32_t R1max = 100000;
 const uint32_t maximumAnalogValue = 4095;
-const uint32_t minimumAnalogValue = 4095 * 10 / 100;
+const uint32_t minimumAnalogValue = 4095 * 10 / 110;
 const uint32_t gameportButtonPins[] = { PA4, PA5, PA6, PA7 };
 const unsigned numGameportButtonPins = sizeof(gameportButtonPins) / sizeof(*gameportButtonPins);
 
@@ -36,8 +38,8 @@ typedef struct {
 JoystickAxis axes[] = {
     { PA0, &HIDJoystick::X, false },
     { PA1, &HIDJoystick::Y, false },
-    { PA2, &HIDJoystick::sliderRight, false },
-    { PA3, &HIDJoystick::Xrotate, false }
+    { PA2, &HIDJoystick::Xrotate, false }, // sliderRight
+    { PA3, &HIDJoystick::sliderRight, true } // Xrotate, false }
 };
 
 const unsigned numAxes = sizeof(axes) / sizeof(*axes);
@@ -61,18 +63,20 @@ void setup()
     
     digitalWrite(LED_BUILTIN, 1);     
     gameportSetup();
+    adc_set_sample_rate(ADC1, ADC_SMPR_13_5); // ADC_SMPR_13_5, ADC_SMPR_1_5
     Joystick.setManualReportMode(true);
 } 
 
-uint16_t remap(uint16_t analogValue, bool value) {
-    uint32_t v = analogValue;
-    if (v < minimumAnalogValue)
-      return 0;
-    else {
-      v = (v - minimumAnalogValue) * 4095 / ( maximumAnalogValue - minimumAnalogValue );
-      if (v > 4095)
-        return 4095;
-      return v;
+uint16_t remap(uint16_t analogValue, bool reverse) {
+    uint32_t v = analogValue/4*4;
+    if (v == 0)
+      return reverse ? 0 : 1023;
+    else { 
+      uint32_t R = R2*4095/v-R2;
+      if (R > R1max)
+        return reverse ? 0 : 1023;
+      else
+        return reverse ? 1023-R*1023/R1max: R*1023/R1max;
     }
 }
 
@@ -81,7 +85,7 @@ void loop()
 {
     uint8_t pressed = 0;
 
-    for (int i=0; i<numGameportButtonPins; i++)
+    for (int i=0; i<numGameportButtonPins; i++) 
        Joystick.button(i+1, gameportButtons[i]->getState());
 
     for (int i=0; i<numAxes; i++)
